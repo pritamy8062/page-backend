8e115ef2f635ec9fd0371be247acc666a9574a81
pritamy8062 <120549375+pritamy8062@users.noreply.github.com>
2023-03-28 23:57:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,17 +22,13 @@ app.use("/api/users", userRoutes);
 // --------------------------deployment------------------------------
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
+
+   
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
+
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
